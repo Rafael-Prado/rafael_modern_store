8f021c0aa67d219423824982f97d73fd052403ae
Rafael da cruz do prado <rafaeldacruzdoprado@hotmail.com>
2018-04-22 18:18:13 -0300

    Adicionando entidades

---
+++ ModernStore.Domain/Entities/Customer.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidator;
+using ModernStore.Shared;
+using System;
+
+namespace ModernStore.Domain.Entities
+{
+    public class Customer: Entity
+    {
+        public Customer(string firstName, string lastName, string email, User user)
+        {            
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = null;
+            Email = email;
+            User = user;
+            // Validação
+            new ValidationContract<Customer>(this)
+                .IsRequired(x => x.FirstName, "Nome é obrigatório")
+                .HasMaxLenght(x => x.FirstName, 60)
+                .HasMinLenght(x => x.FirstName, 3)
+                .IsRequired(x => x.FirstName, "Sobrenome é obrigatório")
+                .HasMaxLenght(x => x.LastName, 60)
+                .HasMinLenght(x => x.LastName, 3)
+                .IsEmail(x => x.Email, "E-mail é invalido");
+
+        }
+        
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public  DateTime? BirthDate{ get; private set; }
+        public string Email { get; private set; }
+        public User User { get; private set; }
+
+        public void Update(string name, DateTime birthDate)
+        {
+            BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+
+    }
+}
---
+++ ModernStore.Domain/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using ModernStore.Shared;
+
+namespace ModernStore.Domain.Entities
+{
+    public class User: Entity
+    {
+        public User(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+            Active = false;
+        }
+
+        public string UserName { get; private set; }
+        public string Password { get; private set; }
+        public bool Active { get; private set; }
+
+        public void Activate() => Active = true;
+        
+        public void Deactivate() => Active = false;
+        
+
+    }
+}
